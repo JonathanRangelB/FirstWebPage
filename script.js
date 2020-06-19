1e392f4695bcb8117b80c8755e59c26196d7e0da
Jonathan Rangel <jonathanrangelb@gmail.com>
2020-06-18 23:31:03 -0500

    arreglando el bug que no permitia al botón de menu desplegar correctamente el mismo

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ ipad.addListener(validation);
 
 function validation(event) {
   if (event.matches) {
-    burgerButton.addEventListener("click", hideShow);
+    burgerButton.addEventListener("click", hideShowMenu);
   } else {
-    burgerButton.removeEventListener("click", hideShow);
+    burgerButton.removeEventListener("click", hideShowMenu);
   }
 }
 
-const hideShow = function () {
+validation(ipad);
+
+function hideShowMenu() {
   menu.classList.toggle("is-active");
-};
+}
+
+/* por cuestiones de hoisting, no puedo llamar a validation(ipad) si la constante hideShowMenu no esta declarada antes */
+// const hideShowMenu = function () {
+//   menu.classList.toggle("is-active");
+// };
