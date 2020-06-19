5e428f34fec12ffb3be18c6b686b8f8d20ed54ac
Jonathan Rangel <jonathanrangelb@gmail.com>
2020-06-18 21:53:17 -0500

    Agregando mas funcionalidad con js para que los listeners solo se carguen en cierto tipo de tamaño de pantalla y no en la resulucion para desktop a la página y corrigiendo la parte de social para pantallas mas pequeñas

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const burgerButton = document.querySelector(".burger-button");
 const menu = document.querySelector(".menu");
 
-burgerButton.addEventListener("click", () => {
+const ipad = window.matchMedia("screen and (max-width: 767px)");
+ipad.addListener(validation);
+
+function validation(event) {
+  if (event.matches) {
+    burgerButton.addEventListener("click", hideShow);
+  } else {
+    burgerButton.removeEventListener("click", hideShow);
+  }
+}
+
+const hideShow = function () {
   menu.classList.toggle("is-active");
-});
+};
